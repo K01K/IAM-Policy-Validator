50d31449973c4100d9ae6498a7d862dc92bbcd8f
K01K <100278741+K01K@users.noreply.github.com>
2024-04-19 16:37:31 +0200

    Update iam.py

--- iam.py
+++ iam.py
@@ -1,15 +1,18 @@
 import json
+import os
 
 def importJSON(filename):
     try:
+        if os.path.exists(filename)==False:
+            raise  FileNotFoundError(f"File not found")
         with open(filename) as json_file:
             return json.load(json_file)
     except json.JSONDecodeError as e:
         raise Exception(f"Invalid JSON data: {e}")
 
 def checkValidity(json_data):
-    for statement in json_data["PolicyDocument"]["Statement"]:
-        if statement.get("Resource") == "*":
+    for check in json_data["PolicyDocument"]["Statement"]:
+        if check.get("Resource") == "*":
             print(False)
         else:
             print(True)
